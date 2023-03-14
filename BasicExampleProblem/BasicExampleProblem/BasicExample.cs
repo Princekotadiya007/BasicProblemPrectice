8a30e2fb5012fa86a48cb5c7c83976b53f16179a
Princekotadiya007 <princuuu77@gmail.com>
2023-03-14 14:07:31 +0530

    [prince]: sum of multiple 3 to 5

--- BasicExampleProblem/BasicExampleProblem/BasicExample.cs
+++ BasicExampleProblem/BasicExampleProblem/BasicExample.cs
@@ -95,5 +95,20 @@ namespace BasicExampleProblem
                 Console.WriteLine(num + " it is not divisible");
             }
         }
+        public void SumOfMultiple()
+        {
+            Console.WriteLine("Enter the Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i =1; i<num; i++)
+            {
+                if(i%3 == 0 || i%5 == 0)
+                {
+                    sum += i;
+                }
+                Console.WriteLine("The sum of all multiples of 3 and 5 is" + sum);
+            }
+        }
     }
 }
--- BasicExampleProblem/BasicExampleProblem/Program.cs
+++ BasicExampleProblem/BasicExampleProblem/Program.cs
@@ -15,7 +15,7 @@ namespace BasicExampleProblem
             while (flag)
             {
                 Console.WriteLine("Basic Problem Example");
-                Console.WriteLine("\n1.Even or Odd,\n2.Odd Number Range, \n3.PositiveOrNot,\n4 largest Number\n5.SwapNumber,\n6.Divisible By 2");
+                Console.WriteLine("\n1.Even or Odd,\n2.Odd Number Range, \n3.PositiveOrNot,\n4 largest Number\n5.SwapNumber,\n6.Divisible By 2,\n7.Sum of multiple 3 to 5");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -38,6 +38,9 @@ namespace BasicExampleProblem
                         basic.Divisible();
                         break;
                     case 7:
+                        basic.SumOfMultiple();
+                        break;
+                    case 8:
                         flag= false;
                         break;
                 }
